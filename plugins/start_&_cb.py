6de96e5d5898d9436446fe4546831e232ab9f542
Shekhar Upadhyay <121297704+SexyShekharXD@users.noreply.github.com>
2023-08-13 14:29:21 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -45,19 +45,19 @@ async def cb_handler(client, query: CallbackQuery):
         await query.message.edit_text(
             text=Txt.HELP_TXT,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[
+            reply_markup=InlineKeyboardMarkup([
                 InlineKeyboardButton("🔒 Cʟᴏꜱᴇ", callback_data = "close"),
                 InlineKeyboardButton("◀️ Bᴀᴄᴋ", callback_data = "start")
-         ]])            
+        ]])            
         )
     elif data == "about":
         await query.message.edit_text(
             text=Txt.ABOUT_TXT.format(client.mention),
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([
+            reply_markup=InlineKeyboardMarkup([[
                 InlineKeyboardButton("🔒 Cʟᴏꜱᴇ", callback_data = "close"),
                 InlineKeyboardButton("◀️ Bᴀᴄᴋ", callback_data = "start")
-         ]])            
+        ]])            
         )
     elif data == "close":
         try:
